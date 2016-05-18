4a87b52812019708c81fc488f4fb9c7738edc7ab
Norberto Caceres <norbe94@hotmail.com>
2016-05-18 15:12:10 -0700

    add js for slideshow, html for imgs and add picture files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+slideShow();
+var slideIndex = 0;
+
+function slideShow(){
+  var i;
+  var pictures = document.getElementByClassName('fotosShow');
+  for(i = 0; i < x.length; i++){
+    pictures[i].style.display = "none";
+  };
+  slideIndex++;
+  if(slideIndex > pictures.length) {slideIndex = 1}
+  pictures[slideIndex-1].style.display = "block";
+  setTimeout(slideShow, 3000); // cambia foto cada 3 segundos
+};
